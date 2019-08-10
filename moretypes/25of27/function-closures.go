694df06cde47b8e9197d566fb3246175ed0355fc
Monika Hrda <50584912+monika-hrda@users.noreply.github.com>
2019-08-10 20:55:40 +0100

    Function closures example with Println statements
    Go functions may be closures. A closure is a function value that references variables from outside its body. The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.

--- moretypes/25of27/function-closures.go
+++ moretypes/25of27/function-closures.go
@@ -4,18 +4,17 @@ import "fmt"
 
 func adder() func(int) int {
 	sum := 0
+	fmt.Println("value of sum is ", sum)
 	return func(x int) int {
+		fmt.Println("value of sum inside of the returned function is ", sum)
 		sum += x
 		return sum
 	}
 }
 
 func main() {
-	pos, neg := adder(), adder()
+	pos := adder()
 	for i := 0; i < 10; i++ {
-		fmt.Println(
-			pos(i),
-			neg(-2*i),
-		)
+		fmt.Println(pos(i))
 	}
 }
